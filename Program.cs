906d57f07df1866c48c40766dba3f9c5de603434
triva <tristan.hasqeunoph@gmail.com>
2020-10-20 15:48:17 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * Exo 4 calcul dans un affichage
+ * Tristan
+ * 08/10/2020
+ */
+using System;
+
+
+namespace Exo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //déclaration
+            float ht, tva;
+
+            //Saisie valeur
+            Console.Write("Entrez le prix HT = ");
+            ht = float.Parse(Console.ReadLine());           
+            Console.Write("Entrez le prix TVA =");
+            tva = float.Parse(Console.ReadLine());
+
+            //calcul et affichage resultat
+            Console.Write("Le prix Total est de = " +(ht*(1+(tva/100)))+"€");
+            Console.ReadLine();
+
+        }
+    }
+}
